fc3a54cbfac012f5f944d96bc26b473cdfbf2f57
webOps-aman <amanhxe@gmail.com>
2024-12-19 17:46:39 +0530

    add new topic connect database using mongodb

---
+++ 8connectdb/utils/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const URI = "mongodb://127.0.0.1:27017/mern_admin";
+
+const connectdb = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("Connected to MongoDB successfully!")
+    } catch (error) {
+        console.error("Could not connect to MongoDB:", error);
+        process.exit(0);
+    }
+};
+
+module.exports = connectdb;
