546d5ed708eb1dfb7220265ae1ce6c2768215c84
webOps-aman <amanhxe@gmail.com>
2024-12-20 19:37:20 +0530

    add new topic middleware using zod

---
+++ 15loginvalidzod/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+const port = 4022;
+const router = require('./router/auth-router');
+const connectdb = require("./utils/db");
+
+app.use(express.json());
+app.use('/api/auth', router);
+
+connectdb().then(() => {
+    app.listen(port, () => {
+        console.log(`Server Listening To The Port ${port}`);
+    });
+});
