7c8debbaa685f15988ebbbe729762b29222d907c
webOps-aman <amanhxe@gmail.com>
2024-12-24 00:14:19 +0530

    add new topic frontend connect with backend

---
+++ 26frontend-connect-backend/client/src/pages/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+  const [user, setUser] = useState({
+      email:"",
+      password:"",
+    });
+
+    const handleInput = (e) => {
+      let name = e.target.name;
+      let value = e.target.value;
+  
+      setUser({
+        ...user,
+        [name]: value,
+  
+      });
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(user);
+    }
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/login.png"
+                  alt=" let's fill the login form "
+                  width="500"
+                  height="500"
+                />
+              </div>
+
+              {/* let tackle registration form  */}
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">login form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  )
+}
+
+export default Login
